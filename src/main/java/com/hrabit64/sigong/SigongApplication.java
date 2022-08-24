df50ae63ca3660bf62aa0d058ae36aa4b808cb9d
hrabit64 <hzser123@hanyang.ac.kr>
2022-08-25 06:46:47 +0900

    first commit

---
+++ src/main/java/com/hrabit64/sigong/SigongApplication.java
@@ -0,0 +1,13 @@
+package com.hrabit64.sigong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrabit64/sigong/SigongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrabit64.sigong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
